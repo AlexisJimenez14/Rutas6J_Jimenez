0c2490dd8eb584c0fe72a6019ad85b4fa2168e15
ALEXIS_FABIAN_JIMENEZ_JIMENEZ <a.22308051281213@cbtis128.edu.mx>
2025-04-08 15:54:49 +0000

    rutas2

---
+++ lib/pagina_dos.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class PantallaDos extends StatelessWidget {
+  const PantallaDos({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Panta dos',
+          style: TextStyle(
+            color: Colors.black, // Color de la letra blanco
+            fontSize: 25.0, // Tamaño de la letra 15
+          ),
+        ),
+        backgroundColor: Color(0xfff2f25f),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Regresar'),
+        ),
+      ),
+    );
+  }
+}
